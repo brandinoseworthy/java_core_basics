2a4a5dd3eec94f8ac005aebaf7b320ce40e7ea25
Brandi Noseworthy <blnoseworthy@gmail.com>
2022-02-24 18:18:31 -0500

    Create Array assignment. JavaBasicsClass now has ArrayExercise methods that can be called upon.

---
+++ ArrayExercises.java
@@ -0,0 +1,73 @@
+package com.teksystems_java;
+
+import java.util.Arrays;
+
+public class ArrayExercises {
+
+
+
+    // 1. Write a program that creates an array of integers with a length of 3.
+    // Assign the values 1, 2, and 3 to the indexes. Print out each array element.
+
+//    public static void exercise1() {
+//        int[] firstArray = {1, 2, 3};
+//        for (int i = 0; i < firstArray.length; i++) {
+//            System.out.println(firstArray[i]);
+//        }
+// Result:
+// 1
+// 2
+// 3
+//
+//Process finished with exit code 0
+//
+//    } // end exercise1 method
+
+    // 2. SKIPPED Write a program that returns the middle element in an array.
+    // Give the following values to the integer array: {13, 5, 7, 68, 2} and produce
+    // the following output: 7
+
+//    public static void exercise2() {
+//        int[] arr2 = {13, 5, 7, 68, 2};
+//        int begining = 0;
+//        int end = arr2.length - 1;
+//        int middle = (begining + end)/2;
+//        for (int i = 0; i < arr2.length; i++){
+//            System.out.println(middle);
+//        }
+//    } // end exercise2 method
+
+// 3.Write a program that creates an array of String type and initializes
+// it with the strings “red”, “green”, “blue” and “yellow”. Print out the array length.
+// Make a copy using the clone( ) method.
+// Use the Arrays.toString( ) method on the new array to verify that the original array was copied.
+
+//    public static void exercise3() {
+//
+//        String [] colors = {"red","green", "blue", "yellow"};
+//        int arrayLength = colors.length;
+//
+//            System.out.println("The length of this colors array is: " + colors.length);
+//
+//            String[] copyOfColors = colors.clone();
+//            System.out.println("The color array was copied into copyOfColors and it also holds these values: " + Arrays.toString(copyOfColors));
+
+// Results:
+//The length of this colors array is: 4
+//The color array was copied into copyOfColors and it also holds these values: [red, green, blue, yellow]
+
+
+ //   } // end exercise3 method
+
+    //4. Write a program that creates an integer array with 5 elements (i.e., numbers).
+    // The numbers can be any integers.  Print out the value at the first index and the last index
+    // using length - 1 as the index. Now try printing the value at index = length
+    // ( e.g., myArray[myArray.length ] ).  Notice the type of exception which is produced.
+    // Now try to assign a value to the array index 5. You should get the same type of exception.
+
+    public static void exercise4() {
+    }
+
+
+
+} // end ArrayExercises Class
