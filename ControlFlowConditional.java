22fb771039b42302649806528bbd189c0321702f
Brandi Noseworthy <blnoseworthy@gmail.com>
2022-02-27 16:08:05 -0500

    Begin Control Flow Conditional Exercises.

---
+++ ControlFlowConditional.java
@@ -0,0 +1,42 @@
+package com.teksystems_java;
+
+import java.util.Scanner;
+
+public class ControlFlowConditional {
+    public static void main(String[] args) {
+
+        // 1. Write a program that declares 1 integer variable x, and then assigns 7 to it.
+        // Write an if statement to print out “Less than 10” if x is less than 10.
+        // Change x to equal 15 and notice the result (console should not display anything).
+
+//        int x = 7;
+//        x = 15; // now nothing prints
+//        if (x < 10)
+//        System.out.println("Less than 10");
+//
+        // Bonus: input a number and have the same scenario perform
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Enter a number: ");
+//        int y = scanner.nextInt();
+//        if (y < 10)
+//            System.out.print("Less than 10");
+
+        // 2. Write a program that declares 1 integer variable x, and then assigns 7 to it.
+        // Write an if-else statement that prints out “Less than 10” if x is less than 10 and “Greater than 10” if x is greater than 10.
+        // Change x to 15 and notice the result.
+
+//            int x = 7;
+//            x = 13; //
+//
+//             if (x < 10)
+//                System.out.println("Less than 10");
+//            else if (x > 10)
+//                System.out.println("Greater than 10");
+
+
+
+    }// end main method
+
+
+} // end ControlFlowConditional Class
